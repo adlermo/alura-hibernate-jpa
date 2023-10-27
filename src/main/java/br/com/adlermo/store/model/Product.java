df4485ecd2166597280bb104e51b03b3ced43749
Adler Mesquita Orteney <adlerorteney@gmail.com>
2023-10-27 14:02:23 -0300

    Created relationship category x product

--- src/main/java/br/com/adlermo/store/model/Product.java
+++ src/main/java/br/com/adlermo/store/model/Product.java
@@ -1,11 +1,13 @@
 package br.com.adlermo.store.model;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.Getter;
@@ -24,4 +26,8 @@ public class Product {
     private String name;
     private String description;
     private BigDecimal price;
+    private LocalDateTime creationDateTime = LocalDateTime.now();
+    
+    @ManyToOne
+    private Category category;
 }
