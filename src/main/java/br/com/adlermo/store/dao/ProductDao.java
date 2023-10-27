3d930b47827521b162d91fe3ab7a2f5b4c3c8804
Adler Mesquita Orteney <adlerorteney@gmail.com>
2023-10-27 14:02:58 -0300

    Added CRUD methods to Product DAO

--- src/main/java/br/com/adlermo/store/dao/ProductDao.java
+++ src/main/java/br/com/adlermo/store/dao/ProductDao.java
@@ -1,5 +1,7 @@
 package br.com.adlermo.store.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
 import br.com.adlermo.store.model.Product;
@@ -14,6 +16,33 @@ public class ProductDao {
 
     public void create(Product product) {
         this.entityManager.persist(product);
+    }
+
+    public List<Product> readAll() {
+        return this.entityManager.createQuery("SELECT p FROM Produto p", Product.class).getResultList();
+    }
+
+    public List<Product> readAllByName(String name) {
+        return this.entityManager.createQuery("SELECT p FROM Produto p WHERE p.name = :name", Product.class)
+                .setParameter("name", name).getResultList();
+    }    
+    
+    public List<Product> readAllByCategory(String category) {
+        return this.entityManager.createQuery("SELECT p FROM Produto p WHERE p.category.name = :category", Product.class)
+                .setParameter("category", category).getResultList();
+    }
+
+    public Product readById(Long id) {
+        return this.entityManager.find(Product.class, id);
+    }
+
+    public void update(Product product) {
+        this.entityManager.merge(product);
+    }
+
+    public void delete(Product product) {
+        product = this.entityManager.merge(product);
+        this.entityManager.remove(product);
     }
 
 }
